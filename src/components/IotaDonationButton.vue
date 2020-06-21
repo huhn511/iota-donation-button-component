64391f2af8ebc054479397a70dcb40ca02d8cac3
huhn511 <sehe89@gmail.com>
2020-06-21 02:48:04 +0200

    :art: refactored button

--- src/components/IotaDonationButton.vue
+++ src/components/IotaDonationButton.vue
@@ -1,8 +1,6 @@
 <template>
    <span id="iota-donation-button">
-    <button class="btn btn-animated" v-if="!qrCodeData" @click="donate">
-     <img class="icon" src="@/assets/iota.svg" >
-    </button>
+    <DonateButton @donate="donate"  v-if="!qrCodeData" />
     <div v-else >
       <img v-if="qrCodeData" :src="qrCodeData.src" alt="QR CODE" />
       <br />
@@ -19,11 +17,12 @@
 <script>
 import * as IotaQR from "@tangle-frost/iota-qr-lib/pkg/iota-qr-lib.js";
 import { clipboard } from "vue-clipboards";
+import DonateButton from "./DonateButton";
 
 export default {
     name: "IotaDonationButton",
   directives: { clipboard },
-
+  components: { DonateButton },
   props: {
     address: {
       required: true,
@@ -67,78 +66,5 @@ export default {
   position: absolute;
   bottom: 10%;
   right: 10%;
-}
-
-.icon {
-  width: 42px;
-  height: 42px;
-}
-
-.btn {
-  background-color: #0fc1b7;
-  border: none;
-  border-radius: 100%;
-  padding: 20px;
-  text-align: center;
-  text-decoration: none;
-  font-size: 16px;
-  margin: 4px 2px;
-}
-
-.btn:link,
-.btn:visited {
-    text-transform: uppercase;
-    text-decoration: none;
-    padding: 15px 40px;
-    display: inline-block;
-    border-radius: 100px;
-    transition: all .2s;
-    position: absolute;
-}
-
-.btn:hover {
-    cursor: pointer;
-    transform: translateY(-3px);
-    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
-}
-
-.btn:active {
-    transform: translateY(-1px);
-    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
-}
-
-.btn::after {
-    content: "";
-    display: inline-block;
-    height: 100%;
-    width: 100%;
-    border-radius: 100px;
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: -1;
-    transition: all .4s;
-}
-
-.btn:hover::after {
-    transform: scaleX(1.4) scaleY(1.6);
-    opacity: 0;
-}
-
-.btn-animated {
-    animation: moveInBottom 5s ease-out;
-    animation-fill-mode: backwards;
-}
-
-@keyframes moveInBottom {
-    0% {
-        opacity: 0;
-        transform: translateY(30px);
-    }
-
-    100% {
-        opacity: 1;
-        transform: translateY(0px);
-    }
 }
 </style>
